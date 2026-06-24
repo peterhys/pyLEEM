19cb211ff8c51b61c4adb4054845fe35948f7669
Peter Sun <peterhys73@outlook.com>
2026-06-24 00:27:04 -0400

    Update analyzer.py

--- pyleem/analyzer.py
+++ pyleem/analyzer.py
@@ -21,40 +21,65 @@ class Analyzer:
     :ivar UViewReader reader: Reader instance for accessing file data.
     """
 
-    def __init__(self, path, reader=UViewReader, roi=None):
-        self.path = path
-        self.name = pathlib.Path(path).stem
-        self.reader = reader(path)
+    def __init__(self, readers, roi=None, onset=0):
+
+        if onset is None:
+            onset = find_onset([reader.image.sum() for reader in readers])
+        self.onset = onset
+
+        self.readers = readers[self.onset :]
         self._process_list = []
         self._analysis_list = []
         self.roi = roi
 
-    @property
-    def metadata(self):
-        """Return the metadata from the reader."""
-        return self.reader.metadata
+    # @property
+    # def metadata(self):
+    #     """Return the metadata from the reader."""
+    #     return self.reader.metadata
 
-    def __lt__(self, other):
-        """Enable sorting by file path."""
-        return self.path < other.path
+    # def __lt__(self, other):
+    #     """Enable sorting by file path."""
+    #     return self.path < other.path
 
-    @property
-    def image(self):
-        """Return the image data from the reader."""
-        return self.reader.image
-
-    def plot_image(self, ax=None):
+    def plot_raw_image(self, index, ax=None):
         """Plot the raw image data.
 
         :param matplotlib.axes.Axes ax: Matplotlib axes object.
+        :param list indices: List of indices to plot.
         """
+
         ax = ax or plt.gca()
-        ax.imshow(self.image, label=self.name)
+        ax.imshow(self.get_raw_image(index), label=self.name)
         ax.set_xlabel("X [pixels]")
         ax.set_ylabel("Y [pixels]")
         ax.set_title("Raw Image Data")
 
         return ax
+
+    def plot_processed_image(self, index, ax=None):
+        """Plot the raw image data.
+
+        :param matplotlib.axes.Axes ax: Matplotlib axes object.
+        :param list indices: List of indices to plot.
+        """
+        ax = ax or plt.gca()
+        ax.imshow(self.get_processed_image(index), label=self.name)
+        ax.set_xlabel("X [pixels]")
+        ax.set_ylabel("Y [pixels]")
+        ax.set_title("Processed Image Data")
+
+        return ax
+
+    # def plot_profile(self, index, ax=None):
+    #     """Plot the profile data.
+
+    #     :param matplotlib.axes.Axes ax: Matplotlib axes object.
+    #     """
+
+    #     ax = ax or plt.gca()
+    #     ax.plot(self.abscissa, self.ordinate, label=self.name)
+    #     ax.set_xlabel(self.abscissa_label)
+    #     ax.set_ylabel(self.ordinate_label)
 
     @property
     def analysis_list(self):
@@ -66,13 +91,16 @@ class Analyzer:
         """Return the processes."""
         return self._process_list
 
-    @property
-    def processed_image(self):
+    def get_processed_image(self, index):
         """Return the processed image."""
-        image = self.image
+        image = self.readers[index].image
         for process in self._process_list:
             image = process(image)
         return image
+
+    def get_raw_image(self, index):
+        """Return the processed images."""
+        return self.readers[index].image
 
     @property
     def analysis_results(self):
@@ -82,10 +110,32 @@ class Analyzer:
             results[analysis.name] = analysis(self)
         return results
 
+    def get_metadata_list(self, key):
+        """Get metadata values from all analyzers.
+
+        :param str key: Metadata key to retrieve.
+        :return: List of metadata values.
+        :rtype: list
+        """
+        return [reader.metadata[key][0] for reader in self.readers]
+
+    def get_process_description(self):
+        """Get the description of the processes."""
+        return [process.__doc__ for process in self._process_list]
+
+    # def get_attribute_list(self, attr_name):
+    #     """Get attribute values from all analyzers.
+
+    #     :param str attr_name: Attribute name to retrieve.
+    #     :return: List of attribute values.
+    #     :rtype: list
+    #     """
+    #     return [getattr(reader, attr_name) for reader in self.readers]
+
     # def profile(self):
     #     """Return the profile."""
     #     if isinstance(self, LineROI):
-    #         return self.roi.read_profile(self.image)
+    #         return self.roi.profile(self.image)
     #     else:
     #         return self.process_profile()
 
@@ -178,95 +228,95 @@ class Analyzer:
 #         return ax
 
 
-class AnalyzerGroup:
-    """Base class for batch analysis of multiple LEEM files.
+# class AnalyzerGroup:
+#     """Base class for batch analysis of multiple LEEM files.
 
-    Manages a collection of analyzer instances for processing multiple
-    data files together.
+#     Manages a collection of analyzer instances for processing multiple
+#     data files together.
 
-    :param list paths: List of paths to LEEM data files.
-    :param type analyzer: Analyzer class to instantiate for each file.
-    :param kwargs: Additional keyword arguments for each analyzer.
+#     :param list paths: List of paths to LEEM data files.
+#     :param type analyzer: Analyzer class to instantiate for each file.
+#     :param kwargs: Additional keyword arguments for each analyzer.
 
-    :ivar list paths: List of file paths.
-    :ivar list analyzers: List of analyzer instances.
-    :ivar list time_intervals: Time intervals in seconds from first acquisition.
-    """
+#     :ivar list paths: List of file paths.
+#     :ivar list analyzers: List of analyzer instances.
+#     :ivar list time_intervals: Time intervals in seconds from first acquisition.
+#     """
 
-    def __init__(self, analyzers, roi=None, onset=0):
+#     def __init__(self, analyzers, roi=None, onset=0):
 
-        if onset is None:
-            onset = self.find_onset()
-        self.onset = onset
+#         if onset is None:
+#             onset = self.find_onset()
+#         self.onset = onset
 
-        self.analyzers = analyzers[self.onset :]
-        self.roi = roi
+#         self.analyzers = analyzers[self.onset :]
+#         self.roi = roi
 
-        self._process_list = []
-        self._analysis_list = []
+#         self._process_list = []
+#         self._analysis_list = []
 
-    @property
-    def process_list(self):
-        """Return the processes."""
-        return self._process_list
+#     @property
+#     def process_list(self):
+#         """Return the processes."""
+#         return self._process_list
 
-    @property
-    def analysis_list(self):
-        """Return the analysis processes."""
-        return self._analysis_list
+#     @property
+#     def analysis_list(self):
+#         """Return the analysis processes."""
+#         return self._analysis_list
 
-    @property
-    def analysis_results(self):
-        """Return the analysis results."""
-        results = {}
-        for analysis in self._analysis_list:
-            results[analysis.name] = analysis(self)
-        return results
+#     @property
+#     def analysis_results(self):
+#         """Return the analysis results."""
+#         results = {}
+#         for analysis in self._analysis_list:
+#             results[analysis.name] = analysis(self)
+#         return results
 
-    def __len__(self):
-        """Return the number of analyzers in the group."""
-        return len(self.analyzers)
+#     def __len__(self):
+#         """Return the number of analyzers in the group."""
+#         return len(self.analyzers)
 
-    def __iter__(self):
-        """Iterate over the analyzers in the group."""
-        return iter(self.analyzers)
+#     def __iter__(self):
+#         """Iterate over the analyzers in the group."""
+#         return iter(self.analyzers)
 
-    def __getitem__(self, index):
-        """Get the analyzer at the specified index."""
-        return self.analyzers[index]
+#     def __getitem__(self, index):
+#         """Get the analyzer at the specified index."""
+#         return self.analyzers[index]
 
-    def get_metadata_list(self, key):
-        """Get metadata values from all analyzers.
+#     def get_metadata_list(self, key):
+#         """Get metadata values from all analyzers.
 
-        :param str key: Metadata key to retrieve.
-        :return: List of metadata values.
-        :rtype: list
-        """
-        return [analyzer.metadata[key][0] for analyzer in self.analyzers]
+#         :param str key: Metadata key to retrieve.
+#         :return: List of metadata values.
+#         :rtype: list
+#         """
+#         return [analyzer.metadata[key][0] for analyzer in self.analyzers]
 
-    def get_attribute_list(self, attr_name):
-        """Get attribute values from all analyzers.
+#     def get_attribute_list(self, attr_name):
+#         """Get attribute values from all analyzers.
 
-        :param str attr_name: Attribute name to retrieve.
-        :return: List of attribute values.
-        :rtype: list
-        """
-        return [getattr(analyzer, attr_name) for analyzer in self.analyzers]
+#         :param str attr_name: Attribute name to retrieve.
+#         :return: List of attribute values.
+#         :rtype: list
+#         """
+#         return [getattr(analyzer, attr_name) for analyzer in self.analyzers]
 
-    def find_onset(self):
-        """Find the onset of the time series.
+#     def find_onset(self):
+#         """Find the onset of the time series.
 
-        In cases that the collection is turned on before the source,
-        first several frames do not have signal. Here we detect through
-        simple gradient analysis where the onset is.
+#         In cases that the collection is turned on before the source,
+#         first several frames do not have signal. Here we detect through
+#         simple gradient analysis where the onset is.
 
-        If the groups are images, we take the sum of the image and then find the onset.
-        If the groups are profiles, we find the onset of the profiles.
+#         If the groups are images, we take the sum of the image and then find the onset.
+#         If the groups are profiles, we find the onset of the profiles.
 
-        :return: Index of the analyzer with the steepest rise.
-        :rtype: int
-        """
+#         :return: Index of the analyzer with the steepest rise.
+#         :rtype: int
+#         """
 
-        profiles = [analyzer.image.sum() for analyzer in self.analyzers]
+#         profiles = [analyzer.image.sum() for analyzer in self.analyzers]
 
-        return find_onset(profiles)
+#         return find_onset(profiles)
